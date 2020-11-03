764c6445af908693e6376fc4b617980a28af7ba2
xkaixiong <xkaixiong@qq.com>
2020-11-03 14:10:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hi</div>
+</template>
+
+<script>
+import HelloWorld from './components/Koi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld:HelloWorld
+  }
+}
+</script>
---
+++ src/components/Koi.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    我的第一个组件Koi
+  </div>
+</template>
