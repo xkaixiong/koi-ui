a28daf80a7fb0a9524d630c7fb24dddb5f0e959b
xkaixiong <xkaixiong@qq.com>
2020-11-03 22:12:54 +0800

    topNav 改为响应式

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -6,6 +6,7 @@
         <li>菜单1</li>
         <li>菜单2</li>
       </ul>
+      <span class="toggleAside"></span>
     </div>
   </div>
 </template>
@@ -29,6 +30,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo{
     max-width: 6em;
     margin-right: auto;
@@ -41,7 +44,13 @@ export default {
       margin: 0 1em ;
     }
   }
+  >.toggleMenu{}
+  @media (max-width:500px){
+    > .menu{display: none;}
+    >.logo{margin: 0 auto;}
+  }
 }
+
 </style>
 
 export {TopNav}
