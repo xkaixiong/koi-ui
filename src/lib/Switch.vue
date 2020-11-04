9fa393dfd58f2a78015b1d38ce79cf4d41e41c5b
xkaixiong <xkaixiong@qq.com>
2020-11-04 16:04:46 +0800

    可以接受 value值 和 input事件

---
+++ src/lib/Switch.vue
@@ -0,0 +1,58 @@
+<template>
+  <button @click="toggle" :class="{checked:value}">
+    <span></span>
+  </button>
+  <div>{{ value }}</div>
+</template>
+
+<script lang="ts">
+
+export default {
+  props: {
+    value: Boolean
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit('input', !props.value);
+    };
+    return {toggle};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: lightgrey;
+  border-radius: $h/2;
+  position: relative;
+}
+
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+  transition: left 0.3s;
+}
+
+button.checked {
+  background: orange;
+  > span {
+    left: calc(95% - #{$h2});
+  }
+}
+
+
+
+button:focus {
+  outline: none;
+}
+</style>
