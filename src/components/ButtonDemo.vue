d4f37254c621eb14894afc9cdfd9b7ed7cc769c9
xkaixiong <xkaixiong@qq.com>
2020-11-05 14:44:14 +0800

    支持 level 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -10,21 +10,52 @@
     </div>
     <h1>示例2</h1>
     <div>
-      <Button size="small">你好</Button>
-      <Button >你好</Button>
-      <Button size="big">你好</Button>
+      <div>
+        <Button size="big">大大大</Button>
+        <Button>普普通</Button>
+        <Button size="small">小小小</Button>
+      </div>
+      <div>
+        <Button theme="link" size="big">大大大</Button>
+        <Button theme="link">普普通</Button>
+        <Button size="small" theme="link">小小小</Button>
+      </div>
+      <div>
+        <Button size="big" theme="text">大大大</Button>
+        <Button theme="text">普普通</Button>
+        <Button size="small" theme="text">小小小</Button>
+      </div>
+    </div>
+    <h1>示例3</h1>
+    <div>
+      <dir>
+        <Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+      </dir>
+      <dir>
+        <Button theme="link" level="main">主要链接按钮</Button>
+        <Button theme="link">普通链接按钮</Button>
+        <Button theme="link" level="danger">危险链接按钮</Button>
+      </dir>
+      <dir>
+        <Button theme="text" level="main">主要文字按钮</Button>
+        <Button theme="text">普通文字按钮</Button>
+        <Button theme="text" level="danger">危险文字按钮</Button>
+      </dir>
     </div>
   </div>
 </template>
 <script lang="ts">
-import Button from '../lib/Button.vue'
+import Button from '../lib/Button.vue';
+
 export default {
-  components:{Button},
-  setup(){
-    const onClick = ()=>{
+  components: {Button},
+  setup() {
+    const onClick = () => {
       console.log('hi');
-    }
-return {onClick}
+    };
+    return {onClick};
   }
-}
+};
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -22,6 +22,14 @@ export default {
       type:String,
       default: "normal",
     },
+    level:{
+      type:String,
+      default:"normal"
+    },
+    disabled:{
+      type:Boolean,
+      default:false
+    }
   },
   setup(props){
     const {theme,size} =props
@@ -73,8 +81,23 @@ $grey: grey;
   &::-moz-focus-inner {
     border: 0;
   }
-  &.koi-theme-text{
-
+  &.koi-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.koi-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
   }
   &.koi-size-big{
     font-size: 24px;
@@ -86,8 +109,82 @@ $grey: grey;
     height: 20px;
     padding: 0 4px;
   }
-  &.koi-theme-button{
-
+  &.koi-theme-button {
+    &.koi-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.koi-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
   }
+  &.koi-theme-link {
+    &.koi-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.koi-theme-text {
+    &.koi-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.koi-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.koi-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.koi-theme-link, &.koi-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .koi-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: koi-spin 1s infinite linear;
+  }
+}
+@keyframes koi-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
