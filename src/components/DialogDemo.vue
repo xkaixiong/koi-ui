06ec5ec4de557063c9a0a7acab3505c4ba18299c
xkaixiong <xkaixiong@qq.com>
2020-11-05 23:17:44 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,20 +1,13 @@
 <template>
-  <div>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-    <p>Dialog 的文档</p>
-  </div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components:{
+    Dialog
+  }
+}
+</script>
